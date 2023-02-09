8cc58b4252686e829bde4b0721c5c34aa504d325
Szenczi Szabolcs <szenczi.szabolcs@ckik.loc>
2023-02-09 13:48:03 +0100

    js köszöntés

---
+++ java.js
@@ -0,0 +1,15 @@
+var date = new Date();
+var hours = date.getHours();
+var day = date.getDay();
+
+var title = "Szép napot!";
+if(hours<= 9)
+{
+title = "Jó reggelt!";
+}
+else
+if(hours>19)
+{
+title="Szép éjszakát!";
+}
+document.querySelector('body h1').innerHTML = title;
